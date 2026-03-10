2e702c05a0745d4f61083aaf82ca0fa45541297b
xujiaming <worker_xjm@outlook.com>
2026-03-10 21:36:38 +0800

    chore: dynamic config by env

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@
 import { defineConfig } from "astro/config";
 
 import cloudflare from "@astrojs/cloudflare";
-
+import process from "node:process";
 import tailwindcss from "@tailwindcss/vite";
-
+const isProduction = process.env.NODE_ENV === "production";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare(),
-  site: "https://rmvs.site/",
+  adapter:isProduction? cloudflare():void 0,
+  site: isProduction ? "https://rmvs.site/" : void 0,
   vite: {
     plugins: [tailwindcss()],
   },
